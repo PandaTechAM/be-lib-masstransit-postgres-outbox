7d563458f493219416a6634dcd1bcd84b7b070f8
MnatsakanMargaryan <MnatsakanMargaryan@MNATS>
2024-04-04 16:27:31 +0400

    final changes

--- src/PandaNuGet/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PandaNuGet;
-
-public class Class1
-{
-    //Refactor CI/CD pipeline
-    //UPDATE README both in the project and outside of project. Outside is for github, inside is for nuget.org
-    //Create main branch and merge in order CI to start its work
-}
